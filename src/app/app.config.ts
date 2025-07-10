e66d2a9663bf95a2ad51b6f6a6fcb19304500da6
AhmedTarek10 <ahmedtarekessa10@gmail.com>
2025-07-11 00:50:17 +0300

    interceptor works

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+
+
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tokenInterceptor } from './core/token.interceptor'; // Import the function
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
-import { TokenInterceptor } from './core/token.interceptor';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+
     provideRouter(routes),
-
-//     provideHttpClient()
-
-    provideHttpClient(), // ✅ just this, without interceptors
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true
-    }
-
+    provideHttpClient(
+      withInterceptors([tokenInterceptor]) // Register the interceptor
+    ),
+    // Remove the old HTTP_INTERCEPTORS provider
   ]
 };
