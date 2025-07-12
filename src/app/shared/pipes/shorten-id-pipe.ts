ab676b7892482aeba599d824b80b1607c287849d
Abdelkader15 <https://github.com/Abdelkader15>
2025-07-12 18:14:21 +0300

    admin manage result

---
+++ src/app/shared/pipes/shorten-id-pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shortenId',
+  standalone: true 
+})
+export class ShortenIdPipe implements PipeTransform {
+  transform(value: string, length: number = 8): string {
+    if (!value) return '';
+    return value.length > length
+      ? `${value.substring(0, length)}...`
+      : value;
+  }
+}
